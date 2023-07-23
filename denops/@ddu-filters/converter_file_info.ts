49fa709356571d885662ce1d0c4ff19a119d2ead
kamecha <masakikameyama81@gmail.com>
2023-07-23 17:52:27 +0900

    formatに沿ってそのまま返すようにした

--- denops/@ddu-filters/converter_file_info.ts
+++ denops/@ddu-filters/converter_file_info.ts
@@ -1,13 +1,37 @@
-import { BaseFilter, DduItem } from "https://deno.land/x/ddu_vim@v3.4.2/types.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.3/file.ts";
+import {
+  BaseFilter,
+  DduItem,
+  DduOptions,
+} from "https://deno.land/x/ddu_vim@v2.8.3/types.ts";
+import { Denops } from "https://deno.land/x/denops_core@v5.0.0/denops.ts";
 
-type Params = Record<never, never>;
+type Params = {
+  format: string;
+};
 
 export class Filter extends BaseFilter<Params> {
-  filter(): Promise<DduItem[]> {
-    return Promise.resolve([]);
+  filter(args: {
+    denops: Denops;
+    options: DduOptions;
+    filterParams: Params;
+    items: DduItem[];
+  }): Promise<DduItem[]> {
+    return Promise.resolve(
+      args.items
+        .map((item: DduItem) => {
+          const text: string = args.filterParams.format
+            .replaceAll("%D", item.display ?? item.word)
+          return {
+            ...item,
+            display: text,
+          };
+        }),
+    );
   }
   params(): Params {
     return {
+      format: "%D",
     };
   }
 }
